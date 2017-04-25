9fc7d3324617f4265167317b275e241cee3cf522
Fernando Mejia <femeji@microsoft.com>
2017-04-25 08:23:47 -0300

    creación de service de contactos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 }
+
+export class Contact {
+  id: number;
+  name: string;
+  email: string;
+}
--- src/app/contacts-service.service.ts
+++ src/app/contacts-service.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Contact } from './app.component';
 
 @Injectable()
 export class ContactsServiceService {
 
-  constructor() { }
+  private apiURL = 'http://ferantoapitest.azurewebsites.net';
+
+  constructor(private http: Http) {}
+
+  getContacts(): Promise< Contact[] > {
+    const _headers = new Headers();
+    _headers.append('Content-Type', 'application/json');
+    _headers.append('ZUMO-API-VERSION', '2.0.0');
+
+    return this.http.get(this.apiURL + '/contacts', {headers : _headers})
+                    .toPromise()
+                    .then(response => response.json().data as Contact[])
+                    .catch(this.handleError);
+  }
+
+  getContactById(id: number) {
+    const url = `${this.apiURL + 'contacts'}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Contact)
+      .catch(this.handleError);
+
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
 
 }
